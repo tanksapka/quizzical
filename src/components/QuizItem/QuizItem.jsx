a008700e722768e234759ac49d7f95230275f238
Tamas Schell <tanksapka@yahoo.com>
2022-02-09 23:56:47 +0100

    Fixing CSS for QuizItem components, and adding click handlers.

--- src/components/QuizItem/QuizItem.jsx
+++ src/components/QuizItem/QuizItem.jsx
@@ -1,16 +1,37 @@
 import "./QuizItem.css";
 
 export default function QuizItem(props) {
+  console.log(props);
+
+  function getClassName(answer) {
+    let cssValue = "answer-item ";
+
+    if (answer === props.selectedAnswer) {
+      if (props.isSubmitted && answer === props.correct_answer) {
+        cssValue = cssValue + "answer-right";
+      } else if (props.isSubmitted && answer !== props.correct_answer) {
+        cssValue = cssValue + "answer-wrong";
+      } else {
+        cssValue = cssValue + "answer-selected";
+      }
+    } else {
+      cssValue = cssValue + (props.isSubmitted ? "answer-disabled" : "answer-regaular");
+    }
+    return cssValue;
+  }
+
+  const answers = props.answers.map((answer) => {
+    return (
+      <li key={answer} className={getClassName(answer)} onClick={() => props.clickHandler(answer)}>
+        {answer}
+      </li>
+    );
+  });
+
   return (
     <section className="quiz-item-container">
-      <p className="question">How would one say goodbye in Spanish?</p>
-      <ul className="answers">
-        <li className="answer-item answer-regaular">Adiós</li>
-        <li className="answer-item answer-selected">Adiós</li>
-        <li className="answer-item answer-disabled">Hola</li>
-        <li className="answer-item answer-right">Au Revoir</li>
-        <li className="answer-item answer-wrong">Salir</li>
-      </ul>
+      <p className="question">{props.question}</p>
+      <ul className="answers">{answers}</ul>
     </section>
   );
 }
--- src/components/QuizScreen/QuizScreen.jsx
+++ src/components/QuizScreen/QuizScreen.jsx
@@ -1,15 +1,54 @@
 import "./QuizScreen.css";
 import QuizItem from "../QuizItem/QuizItem";
+import { useEffect, useState } from "react";
+
+function decodeHtml(html) {
+  const txt = document.createElement("textarea");
+  txt.innerHTML = html;
+  return txt.value;
+}
 
 export default function QuizScreen(props) {
+  const [questions, setQuestions] = useState([]);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
+      .then((resp) => resp.json())
+      .then((data) => data.results)
+      .then((arr) =>
+        arr.map((q) => ({
+          question: decodeHtml(q.question),
+          correct_answer: decodeHtml(q.correct_answer),
+          answers: [...q.incorrect_answers, q.correct_answer]
+            .map((value) => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => decodeHtml(value)),
+          selectedAnswer: null,
+        }))
+      )
+      .then((finalData) => setQuestions(finalData));
+  }, []);
+
+  const quizItemList = questions.map((q, idx) => (
+    <QuizItem
+      key={idx}
+      isSubmitted={isSubmitted}
+      clickHandler={(answer) =>
+        setQuestions((oldQuestions) =>
+          oldQuestions.map((oldQ) => (oldQ.question === q.question ? { ...oldQ, selectedAnswer: answer } : oldQ))
+        )
+      }
+      {...q}
+    />
+  ));
+
   return (
     <main className="quiz-container">
-      <QuizItem />
-      <QuizItem />
-      <QuizItem />
-      <QuizItem />
-      <QuizItem />
-      <button className="check-answers">Check answers</button>
+      {quizItemList}
+      <button className="check-answers" onClick={() => setIsSubmitted((oldValue) => !oldValue)}>
+        Check answers
+      </button>
     </main>
   );
 }
